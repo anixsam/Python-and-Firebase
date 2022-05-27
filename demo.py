23d916a5105b5a7ba75dcafcc3b688ff4adc3b3c
Anix Sam Saji <70740206+anixsam@users.noreply.github.com>
2022-05-27 11:14:48 +0530

    First Commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCiM23oQNuwil8C1E8Cuz_-7SFqq28MC4c",
+  authDomain: "plutoverse-database.firebaseapp.com",
+  databaseURL: "https://plutoverse-database-default-rtdb.firebaseio.com",
+  projectId: "plutoverse-database",
+  storageBucket: "plutoverse-database.appspot.com",
+  messagingSenderId: "538455885356",
+  appId: "1:538455885356:web:09804aed5c5d967176f389",
+  measurementId: "G-9XHKKCBXZF"
+};
